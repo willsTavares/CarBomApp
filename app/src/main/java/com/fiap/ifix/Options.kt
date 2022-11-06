e7576f7bc346061ec95e4eb7b28e4a48cd006fa4
Willians Tavares <willians.tavares95@hotmail.com>
2022-11-06 17:20:58 -0300

    feat: Add circle img header

--- app/src/main/java/com/fiap/ifix/Options.kt
+++ app/src/main/java/com/fiap/ifix/Options.kt
@@ -1,10 +1,16 @@
 package com.fiap.ifix
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.fragment.app.Fragment
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,26 +40,35 @@ class Options : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_options, container, false)
+        val view = inflater.inflate(R.layout.fragment_options, container, false)
+        val nextBtnChats : RelativeLayout = view.findViewById(R.id.relativeChats)
+        val nextBtnNotification : RelativeLayout = view.findViewById(R.id.relativeNotifications)
+        val nextBtnPayment : RelativeLayout = view.findViewById(R.id.relativePayment)
+        val nextBtnVoucher : RelativeLayout = view.findViewById(R.id.relativeVoucher)
+
+        nextBtnChats.setOnClickListener  {
+            val i = Intent(activity, StillWorking()::class.java)
+            startActivity(i)
+            (activity as Activity?)!!.overridePendingTransition(0, 0)
+        }
+        nextBtnNotification.setOnClickListener  {
+            val i = Intent(activity, StillWorking()::class.java)
+            startActivity(i)
+            (activity as Activity?)!!.overridePendingTransition(0, 0)
+        }
+        nextBtnPayment.setOnClickListener  {
+            val i = Intent(activity, StillWorking()::class.java)
+            startActivity(i)
+            (activity as Activity?)!!.overridePendingTransition(0, 0)
+        }
+        nextBtnVoucher.setOnClickListener  {
+            val i = Intent(activity, StillWorking()::class.java)
+            startActivity(i)
+            (activity as Activity?)!!.overridePendingTransition(0, 0)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Options.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Options().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
